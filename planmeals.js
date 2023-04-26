5de1a69c8430dd1dad6b2c76c5962eb58bd3c8df
isakmd <61452704+isakmd@users.noreply.github.com>
2023-04-26 12:45:36 +0200

    byttet link

--- planmeals.js
+++ planmeals.js
@@ -1,11 +1,11 @@
 
 
-$.getJSON("https://gist.githubusercontent.com/isakmd/f09c90e170f342745e94c6bccdef0895/raw/c52b2481931ac279da5ed7beb6f008882e4763d2/mealplans.json", function (result) {
+$.getJSON("https://gist.githubusercontent.com/isakmd/a5cfc2f513dfc58b4546310d66ad4b3b/raw/b0b6935a143b6484f31d3c8207d539af664141dc/mealplans.json", function (result) {
     console.log(result)
     titlemeal = document.getElementsByClassName("card-title")
     calories = document.getElementsByClassName("card-calories")
     for (i = 0; i < 2; i++) {
-        titlemeal[i].innerHTML = result.Vegan.Lunch[i].Meal
+        titlemeal[i].innerHTML = result.Vegan.Breakfast[i].Meal
         calories[i].innerHTML = result.Vegan.Lunch[i].Nutrition.Calories + " Calories";
 
     }
