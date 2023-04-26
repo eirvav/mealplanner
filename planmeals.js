0a1957ed00c2680035901294c24ea8dcfc7ce98f
isakmd <61452704+isakmd@users.noreply.github.com>
2023-04-26 12:27:46 +0200

    småe endringer med noen css problemer

--- planmeals.js
+++ planmeals.js
@@ -2,10 +2,13 @@
 
 $.getJSON("https://gist.githubusercontent.com/isakmd/f09c90e170f342745e94c6bccdef0895/raw/c52b2481931ac279da5ed7beb6f008882e4763d2/mealplans.json", function (result) {
     console.log(result)
-    titlemeal = document.getElementById("title")
-    titlemeal.innerHTML = result.Vegan.Breakfast[0]
+    titlemeal = document.getElementsByClassName("card-title")
+    calories = document.getElementsByClassName("card-calories")
+    for (i = 0; i < 2; i++) {
+        titlemeal[i].innerHTML = result.Vegan.Lunch[i].Meal
+        calories[i].innerHTML = result.Vegan.Lunch[i].Nutrition.Calories + " Calories";
+
+    }
 })
 
-window.onload = function () {
 
-}
