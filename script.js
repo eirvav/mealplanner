69a7478b165db512ec4962999e056c381bd66484
isakmd <61452704+isakmd@users.noreply.github.com>
2023-04-28 12:58:07 +0200

    La til bilder
    Mangler bare å fikse at bildene ikke strekker seg

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ document
     // console.log("Daily Calorie Intake:", dailyCalories);
 
     // GET THE JSON FOR WEB AND STARTS FUNCTION WITH ALL THE NESSESERY VARIABLES
-    $.getJSON("https://gist.githubusercontent.com/isakmd/a5cfc2f513dfc58b4546310d66ad4b3b/raw/b0b6935a143b6484f31d3c8207d539af664141dc/mealplans.json", function (result) {
+    $.getJSON("https://gist.githubusercontent.com/isakmd/f09c90e170f342745e94c6bccdef0895/raw/e4b70015cfba9e78ce591e32e857241db891ab74/mealplans.json", function (result) {
       console.log(result)
       // Displays the hidden content
       let showresult = document.getElementById("mealplan-container");
@@ -72,20 +72,24 @@ document
 
 function getBreakfast(result, mealType) {
   console.log(mealType)
-  titlemeal = document.getElementsByClassName("title-meal-breakfast")
-  calories = document.getElementsByClassName("calories-breakfast")
+  titlemeal = document.getElementsByClassName("title-meal-breakfast");
+  calories = document.getElementsByClassName("calories-breakfast");
+  imgchange = document.getElementsByClassName("img-breakfast");
   if (mealType == 'All') {
     for (i = 0; i < result.All.Breakfast.length; i++) {
+      imgchange[i].src = result.All.Breakfast[i].Img;
       titlemeal[i].innerHTML = result.All.Breakfast[i].Meal;
       calories[i].innerHTML = result.All.Breakfast[i].Nutrition.Calories + " Calories | " + result.All.Breakfast[i].Nutrition.Protein + " Protein | " + result.All.Breakfast[i].Nutrition.Fat + " Fat";
     }
   } else if (mealType == 'Paleo') {
     for (i = 0; i < result.Paleo.Breakfast.length; i++) {
+      imgchange[i].src = result.Paleo.Breakfast[i].Img;
       titlemeal[i].innerHTML = result.Paleo.Breakfast[i].Meal;
       calories[i].innerHTML = result.Paleo.Breakfast[i].Nutrition.Calories + " Calories | " + result.All.Breakfast[i].Nutrition.Protein + " Protein | " + result.All.Breakfast[i].Nutrition.Fat + " Fat";
     }
   } else if (mealType == 'Vegan') {
     for (i = 0; i < result.Vegan.Breakfast.length; i++) {
+      imgchange[i].src = result.Vegan.Breakfast[i].Img;
       titlemeal[i].innerHTML = result.Vegan.Breakfast[i].Meal;
       calories[i].innerHTML = result.Vegan.Breakfast[i].Nutrition.Calories + " Calories | " + result.All.Breakfast[i].Nutrition.Protein + " Protein | " + result.All.Breakfast[i].Nutrition.Fat + " Fat";
     }
@@ -93,20 +97,24 @@ function getBreakfast(result, mealType) {
 }
 
 function getLunch(result, mealType) {
-  titlemeal = document.getElementsByClassName("title-meal-lunch")
-  calories = document.getElementsByClassName("calories-lunch")
+  titlemeal = document.getElementsByClassName("title-meal-lunch");
+  calories = document.getElementsByClassName("calories-lunch");
+  imgchange = document.getElementsByClassName("img-lunch");
   if (mealType == 'All') {
     for (i = 0; i < result.All.Lunch.length; i++) {
+      imgchange[i].src = result.All.Lunch[i].Img;
       titlemeal[i].innerHTML = result.All.Lunch[i].Meal;
       calories[i].innerHTML = result.All.Lunch[i].Nutrition.Calories + " Calories | " + result.All.Lunch[i].Nutrition.Protein + " Protein | " + result.All.Lunch[i].Nutrition.Fat + " Fat";
     }
   } else if (mealType == 'Paleo') {
     for (i = 0; i < result.Paleo.Lunch.length; i++) {
+      imgchange[i].src = result.Paleo.Lunch[i].Img;
       titlemeal[i].innerHTML = result.Paleo.Lunch[i].Meal;
       calories[i].innerHTML = result.Paleo.Lunch[i].Nutrition.Calories + " Calories | " + result.All.Lunch[i].Nutrition.Protein + " Protein | " + result.All.Lunch[i].Nutrition.Fat + " Fat";
     }
   } else if (mealType == 'Vegan') {
     for (i = 0; i < result.Vegan.Lunch.length; i++) {
+      imgchange[i].src = result.Vegan.Lunch[i].Img;
       titlemeal[i].innerHTML = result.Vegan.Lunch[i].Meal;
       calories[i].innerHTML = result.Vegan.Lunch[i].Nutrition.Calories + " Calories | " + result.All.Lunch[i].Nutrition.Protein + " Protein | " + result.All.Lunch[i].Nutrition.Fat + " Fat";
     }
@@ -123,7 +131,8 @@ function clickMeal(btnNumber, mealday, mealType) {
     : null;
   console.log(mealType)
   console.log(mealday)
-  $.getJSON("https://gist.githubusercontent.com/isakmd/a5cfc2f513dfc58b4546310d66ad4b3b/raw/b0b6935a143b6484f31d3c8207d539af664141dc/mealplans.json", function (result) {
+
+  $.getJSON("https://gist.githubusercontent.com/isakmd/f09c90e170f342745e94c6bccdef0895/raw/e4b70015cfba9e78ce591e32e857241db891ab74/mealplans.json", function (result) {
     if (mealday == "Lunch") {
       var newDiv = document.createElement('div');
       newDiv.className = "added-meals";
@@ -131,14 +140,12 @@ function clickMeal(btnNumber, mealday, mealType) {
         newDiv.innerHTML = result.All.Lunch[btnNumber].Meal + "\n";
         //newDiv.innerHTML += result.Paleo.Lunch[btnNumber].Nutrition.Calories + "g Calories | " + result.All.Lunch[btnNumber].Nutrition.Protein + "g Protein | " + result.All.Lunch[btnNumber].Nutrition.Fat + "g Fat";
         for (var i = 0; i < result.All.Lunch[btnNumber].Ingredients.length; i++) {
-
           ingredientArray.push(result.All.Lunch[btnNumber].Ingredients[i]);
         }
         ingredients()
       } else if (mealType == 'Paleo') {
         newDiv.textContent = result.Paleo.Lunch[btnNumber].Meal;
         for (var i = 0; i < result.Paleo.Lunch[btnNumber].Ingredients.length; i++) {
-
           ingredientArray.push(result.Paleo.Lunch[btnNumber].Ingredients[i]);
         }
         ingredients()
