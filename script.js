01fdcacf48982270695b6f05917d50294939d06a
eirvav <eirik.vavik@hotmail.no>
2023-04-28 16:54:43 +0200

    fiksa oppsett

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //Hides the meal selector before it displayed after "submit"
-// window.onload = function () {
-//   let hideresult = document.getElementById("mealplan-container");
-//   hideresult.style.visibility = "hidden";
-// };
+window.onload = function () {
+  let hideresult = document.getElementById("mealplan-container");
+  hideresult.style.visibility = "hidden";
+};
 
-document.querySelectorAll(".card").forEach((card) => {
+document.querySelectorAll(".kort").forEach((card) => {
   card.addEventListener("click", (e) => {
     // Remove the card-selected class from any previously selected card
     document.querySelectorAll(".card-selected").forEach((selectedCard) => {
