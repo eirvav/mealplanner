01d7c1c22d553b1c00cd56a882ca2942b6ed8867
eirvav <eirik.vavik@hotmail.no>
2023-05-15 14:41:25 +0200

    little fixes
    removed the index.global.js file,added <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.7/index.global.min.js'></script>

--- assets/js/cal.js
+++ assets/js/cal.js
@@ -87,3 +87,32 @@ document.addEventListener("DOMContentLoaded", function () {
   });
   calendar.render();
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  var calendarEl = document.getElementById("calendar");
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    eventClick: function (info) {
+      var eventObj = info.event;
+
+      if (eventObj.url) {
+        alert(
+          "Clicked " +
+            eventObj.title +
+            ".\n" +
+            "Will open " +
+            eventObj.url +
+            " in a new tab"
+        );
+
+        window.open(eventObj.url);
+
+        info.jsEvent.preventDefault(); // prevents browser from following link in current tab.
+      } else {
+        alert("Clicked " + eventObj.title);
+      }
+    },
+  });
+
+  calendar.render();
+});
