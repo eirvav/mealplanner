b5441f5761c677ffb8508fe6f6163bfe1951da59
eirvav <eirik.vavik@hotmail.no>
2023-05-16 18:12:14 +0200

    pdf file - removed keys

--- GetGitData.bat
+++
@@ -1,1 +0,0 @@
-git log --pretty="format:[START commit][author=%%an][time=%%at][message=%%s][hash=%%H]" --shortstat > git-data.txt
--- getGitDataMac.sh
+++
@@ -1,6 +0,0 @@
-<<<<<<< Updated upstream
-git log --oneline --decorate --graph --all --pretty="format:[START commit][author=%an][time=%at][message=%s][hash=%H]" --shortstat > git-data.txt
-=======
-git log --pretty="format:[START commit][author=%an][time=%at][message=%s][hash=%H]" --shortstat > git-data.txt
-
->>>>>>> Stashed changes
